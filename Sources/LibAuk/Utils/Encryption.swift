6390be12e994f0e8ee5a07a0cca287b3f7e29315
hvthhien <hvthhien@gmail.com>
2021-10-11 14:41:11 +0700

    update platform

--- Sources/LibAuk/Utils/Encryption.swift
+++ Sources/LibAuk/Utils/Encryption.swift
@@ -11,20 +11,20 @@ import CryptoKit
 
 enum Encryption {
     
-    static func encrypt(_ data: Data, _ passcode: String) throws -> Data {
-        let inputKey = SymmetricKey(data: passcode.utf8)
-        let hkdfResultKey = HKDF<SHA256>.deriveKey(inputKeyMaterial: inputKey, outputByteCount: 64)
-        
-        return try ChaChaPoly.seal(data, using: hkdfResultKey).combined
-    }
-    
-    static func decrypt(_ data: Data, _ passcode: String) throws -> Data {
-        let box = try ChaChaPoly.SealedBox.init(combined: data)
-                
-        let inputKey = SymmetricKey(data: passcode.utf8)
-        let hkdfResultKey = HKDF<SHA256>.deriveKey(inputKeyMaterial: inputKey, outputByteCount: 64)
-        
-        return try ChaChaPoly.open(box, using: hkdfResultKey)
-    }
+//    static func encrypt(_ data: Data, _ passcode: String) throws -> Data {
+//        let inputKey = SymmetricKey(data: passcode.utf8)
+//        let hkdfResultKey = HKDF<SHA256>.deriveKey(inputKeyMaterial: inputKey, outputByteCount: 64)
+//        
+//        return try ChaChaPoly.seal(data, using: hkdfResultKey).combined
+//    }
+//    
+//    static func decrypt(_ data: Data, _ passcode: String) throws -> Data {
+//        let box = try ChaChaPoly.SealedBox.init(combined: data)
+//                
+//        let inputKey = SymmetricKey(data: passcode.utf8)
+//        let hkdfResultKey = HKDF<SHA256>.deriveKey(inputKeyMaterial: inputKey, outputByteCount: 64)
+//        
+//        return try ChaChaPoly.open(box, using: hkdfResultKey)
+//    }
     
 }
